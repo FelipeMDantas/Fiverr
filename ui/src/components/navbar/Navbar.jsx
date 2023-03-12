4ba300e6c873b1ec3f40f04129fd6fa6c9e1da36
FelipeMDantas <felipemdantas@terra.com.br>
2023-03-11 22:58:29 -0300

    Navbar component stylized.

---
+++ ui/src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">fiverr</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign In</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+
+      <hr />
+    </div>
+  );
+};
+
+export default Navbar;
