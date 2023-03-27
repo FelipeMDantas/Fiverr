2a1c8e606f26a319e3a6906c9b8e2cfe837358f1
FelipeMDantas <felipemdantas@terra.com.br>
2023-03-26 22:06:03 -0300

    Message page developed and Add page structured.

--- ui/src/pages/add/Add.jsx
+++ ui/src/pages/add/Add.jsx
@@ -1,7 +1,29 @@
 import "./Add.scss";
 
 const Add = () => {
-  return <div className="add">Add</div>;
+  return (
+    <div className="add">
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              placeholder="e.g.: I will do something i'm really good at"
+            />
+            <label htmlFor="">Category</label>
+            <select name="cats" id="cats">
+              <option value="design">Design</option>
+              <option value="web">Web Development</option>
+              <option value="animation">Animation</option>
+              <option value="music">Music</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
--- ui/src/pages/message/Message.jsx
+++ ui/src/pages/message/Message.jsx
@@ -10,9 +10,55 @@ const Message = () => {
         </span>
         <div className="messages">
           <div className="item">
-            <img src="" alt="" />
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
           </div>
         </div>
+        <hr />
         <div className="write">
           <textarea
             name=""
