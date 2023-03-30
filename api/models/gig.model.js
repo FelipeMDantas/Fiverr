33f26635dee747ad65b76c0d61727fd166ba4800
FelipeMDantas <felipemdantas@terra.com.br>
2023-03-29 23:36:47 -0300

    Gig and user schema models added to the project.

---
+++ api/models/gig.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const gigSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required,
+    },
+    title: {
+      type: String,
+      required,
+    },
+    desc: {
+      type: String,
+      required,
+    },
+    totalStars: {
+      type: Number,
+      required,
+    },
+    starNumber: {
+      type: Number,
+      required,
+    },
+    cat: {
+      type: String,
+      required,
+    },
+    price: {
+      type: Number,
+      required,
+    },
+    cover: {
+      type: String,
+      required,
+    },
+    images: {
+      type: [String],
+      required,
+    },
+    shortTitle: {
+      type: String,
+      required,
+    },
+    shortDesc: {
+      type: String,
+      required,
+    },
+    deliveryTime: {
+      type: Number,
+      required,
+    },
+    revisionNumber: {
+      type: Number,
+      required,
+    },
+    features: {
+      type: [String],
+      required,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Gig", gigSchema);
--- api/server.js
+++ api/server.js
@@ -6,13 +6,16 @@ const app = express();
 dotenv.config();
 mongoose.set("strictQuery", true);
 
-try {
-  await mongoose.connect(process.env.MONGO);
-  console.log("Connected to mongoDB!");
-} catch (error) {
-  console.log(error);
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.listen(8000, () => {
+  connect();
   console.log("Backend server is running!");
 });
