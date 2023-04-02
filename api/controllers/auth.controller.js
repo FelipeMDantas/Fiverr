24f4fe8a3ed448b99fa7c8d5b59f6ecb4a16a8ba
FelipeMDantas <felipemdantas@terra.com.br>
2023-04-01 23:20:40 -0300

    Password encription established.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,14 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
 
     await newUser.save();
     res.status(201).send("The user has been created.");
@@ -11,6 +17,23 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(404).send("User not found.");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(400).send("Wrong password or username.");
+
+    const token = jwt.sign({
+      id: user._id,
+    });
+
+    const { password, ...info } = user._doc;
+    res.status(200).send(info);
+  } catch (err) {
+    res.status(500).send("Something went wrong.");
+  }
+};
 
 export const logout = async (req, res) => {};
