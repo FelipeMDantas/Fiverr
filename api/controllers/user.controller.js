d215bf712d4286b56e01cc87ca8b53291e527a77
FelipeMDantas <felipemdantas@terra.com.br>
2023-04-02 21:45:07 -0300

    Authentication and account deletion established.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,2 +1,17 @@
-export const fn = (req, res) => {
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).send("You are not authenticated.");
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (payload.id !== user._id.toString())
+      return res.status(403).send("You can delete your account only.");
+
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("Account deleted.");
+  });
 };
