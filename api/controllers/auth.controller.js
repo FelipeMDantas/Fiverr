ba667b088b70e1f2ab48b290f11bb1dcbe239d2f
FelipeMDantas <felipemdantas@terra.com.br>
2023-04-03 23:57:04 -0300

    JWT verification middleware created.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
@@ -13,14 +13,18 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).send("The user has been created.");
   } catch (err) {
-    res.status(500).send("Something went wrong.");
+    next(err);
   }
 };
 
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(404).send("User not found.");
+    const err = new Error();
+    err.status = 404;
+    err.message = "User not found!";
+
+    if (!user) return next(err);
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) return res.status(400).send("Wrong password or username.");
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,17 +1,11 @@
-import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 export const deleteUser = async (req, res) => {
   const user = await User.findById(req.params.id);
-  const token = req.cookies.accessToken;
 
-  if (!token) return res.status(401).send("You are not authenticated.");
+  if (req.userId !== user._id.toString())
+    return res.status(403).send("You can delete your account only.");
 
-  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (payload.id !== user._id.toString())
-      return res.status(403).send("You can delete your account only.");
-
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).send("Account deleted.");
-  });
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("Account deleted.");
 };
