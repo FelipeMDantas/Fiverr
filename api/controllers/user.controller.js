c3d54c8cec7f43dec79bb008de0d5398d36b089b
FelipeMDantas <felipemdantas@terra.com.br>
2023-04-05 23:40:28 -0300

    Logout-related request coded.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
   if (req.userId !== user._id.toString())
-    return res.status(403).send("You can delete your account only.");
+    return next(createError(403, "You can delete your account only."));
 
   await User.findByIdAndDelete(req.params.id);
   res.status(200).send("Account deleted.");
