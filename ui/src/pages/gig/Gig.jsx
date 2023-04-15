7860afa9d683e8ca4e6f3fbe41063978b5098217
FelipeMDantas <felipemdantas@terra.com.br>
2023-04-14 21:26:49 -0300

    Gig component further componentized and review controller incremented.

--- ui/src/pages/gig/Gig.jsx
+++ ui/src/pages/gig/Gig.jsx
@@ -3,6 +3,7 @@ import "./Gig.scss";
 import { Slider } from "infinite-react-carousel/lib";
 import { useParams } from "react-router-dom";
 import newRequest from "../../utils/newRequest";
+import Reviews from "../../components/reviews/Reviews";
 
 const Gig = () => {
   const { id } = useParams();
@@ -15,7 +16,11 @@ const Gig = () => {
       }),
   });
 
-  const { isLoadingUser, errorUser, dataUser } = useQuery({
+  const {
+    isLoading: isLoadingUser,
+    error: errorUser,
+    data: dataUser,
+  } = useQuery({
     queryKey: ["user"],
     queryFn: () =>
       newRequest.get(`/users/${data.userId}`).then((res) => {
@@ -122,140 +127,7 @@ const Gig = () => {
                 </div>
               </div>
             )}
-            <div className="reviews">
-              <h2>Reviews</h2>
-              <div className="item">
-                <div className="user">
-                  <img
-                    src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt=""
-                    className="pp"
-                  />
-                  <div className="info">
-                    <span>John Doe</span>
-                    <div className="country">
-                      <img
-                        src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                        alt=""
-                      />
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  I just want to say that art_with_ai was the first, and after
-                  this, the only artist Ill be using on Fiverr. Communication
-                  was amazing, each and every day he sent me images that I was
-                  free to request changes to. They listened, understood, and
-                  delivered above and beyond my expectations. I absolutely
-                  recommend this gig, and know already that Ill be using it
-                  again very very soon
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-              </div>
-              <hr />
-              <div className="item">
-                <div className="user">
-                  <img
-                    src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt=""
-                    className="pp"
-                  />
-                  <div className="info">
-                    <span>John Doe</span>
-                    <div className="country">
-                      <img
-                        src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                        alt=""
-                      />
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  I just want to say that art_with_ai was the first, and after
-                  this, the only artist Ill be using on Fiverr. Communication
-                  was amazing, each and every day he sent me images that I was
-                  free to request changes to. They listened, understood, and
-                  delivered above and beyond my expectations. I absolutely
-                  recommend this gig, and know already that Ill be using it
-                  again very very soon
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-              </div>
-              <hr />
-              <div className="item">
-                <div className="user">
-                  <img
-                    src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt=""
-                    className="pp"
-                  />
-                  <div className="info">
-                    <span>John Doe</span>
-                    <div className="country">
-                      <img
-                        src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                        alt=""
-                      />
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  I just want to say that art_with_ai was the first, and after
-                  this, the only artist Ill be using on Fiverr. Communication
-                  was amazing, each and every day he sent me images that I was
-                  free to request changes to. They listened, understood, and
-                  delivered above and beyond my expectations. I absolutely
-                  recommend this gig, and know already that Ill be using it
-                  again very very soon
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-              </div>
-            </div>
+            <Reviews gigId={id} />
           </div>
           <div className="right">
             <div className="price">
