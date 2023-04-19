05af0aabec03f26d5cf6d57ec1eebc35ed07e2e7
FelipeMDantas <felipemdantas@terra.com.br>
2023-04-18 23:41:30 -0300

    Conversation routes defined.

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,7 +1,17 @@
 import express from "express";
+import {
+  createConversation,
+  getConversations,
+  getSingleConversation,
+  updateConversation,
+} from "../controllers/conversation.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/test");
+router.get("/", verifyToken, getConversations);
+router.get("/", verifyToken, createConversation);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.get("/:id", verifyToken, updateConversation);
 
 export default router;
--- ui/src/pages/orders/Orders.jsx
+++ ui/src/pages/orders/Orders.jsx
@@ -1,107 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
 import "./Orders.scss";
 import { Link } from "react-router-dom";
+import newRequest from "../../utils/newRequest";
 
 const Orders = () => {
-  const currentUser = {
-    id: 1,
-    username: "John Doe",
-    isSeller: true,
-  };
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () =>
+      newRequest.get(`/orders`).then((res) => {
+        return res.data;
+      }),
+  });
 
   return (
     <div className="orders">
-      <div className="container">
-        <div className="title">
-          <h1>Orders</h1>
-          <Link to="/add">
-            <button>Add New Gig</button>
-          </Link>
+      {isLoading ? (
+        "loading"
+      ) : error ? (
+        "Something went wrong."
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Orders</h1>
+            <Link to="/add">
+              <button>Add New Gig</button>
+            </Link>
+          </div>
+          <table>
+            <tr>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Contact</th>
+            </tr>
+            {data.map((order) => (
+              <tr key={order._id}>
+                <td>
+                  <img src={order.img} alt="" className="img" />
+                </td>
+                <td>{order.title}</td>
+                <td>{order.price}</td>
+                <td>
+                  <img src="/img/message.png" alt="" className="delete" />
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
-            <th>Contact</th>
-          </tr>
-          <tr>
-            <td>
-              <img
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-                className="img"
-              />
-            </td>
-            <td>Gig1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img src="/img/message.png" alt="" className="delete" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-                className="img"
-              />
-            </td>
-            <td>Gig1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img src="/img/message.png" alt="" className="delete" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-                className="img"
-              />
-            </td>
-            <td>Gig1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img src="/img/message.png" alt="" className="delete" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-                className="img"
-              />
-            </td>
-            <td>Gig1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img src="/img/message.png" alt="" className="delete" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-                className="img"
-              />
-            </td>
-            <td>Gig1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img src="/img/message.png" alt="" className="delete" />
-            </td>
-          </tr>
-        </table>
-      </div>
+      )}
     </div>
   );
 };
